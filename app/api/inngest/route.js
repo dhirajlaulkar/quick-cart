2bd92a9ecf2e737637d5bc30a1e9a79a4cb8a391
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-06-27 13:13:09 +0530

    Initial commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,14 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/config/inngest";
+import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
+
+
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
