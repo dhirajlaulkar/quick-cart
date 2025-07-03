5f8caf49707f81b9fc9f5c18a31965caa0832418
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-07-03 11:59:08 +0530

    frontend fix

---
+++ .next/static/chunks/app_layout_c0237562.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_10e3cfb1._.js",
+  "static/chunks/node_modules_6054e280._.js",
+  "static/chunks/_aee250b5._.js",
+  "static/chunks/[root-of-the-server]__cdd99c7b._.css"
+],
+    source: "dynamic"
+});
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            QuickCart is your one-stop solution for all your shopping needs. We
+            offer a wide range of products at unbeatable prices, ensuring you
+            get the best value for your money. Shop with us today and experience
+            the convenience of online shopping like never before.
           </p>
         </div>
 
@@ -41,13 +41,13 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
               <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>contact@quick-cart.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © GreatStack.dev All Right Reserved.
+        Copyright 2025 © QuickCart. All Right Reserved.
       </p>
     </footer>
   );
